4c35cffe36af6fec3811d708213de5a0ea3644df
Topbit Du <topbit.du@gmail.com>
2017-03-16 13:19:45 +0800

    1, Improve the Price spec for the validations on the #amount attribute.

--- lib/rspec/models/unidom/price/price_spec.rb
+++ lib/rspec/models/unidom/price/price_spec.rb
@@ -22,27 +22,8 @@ describe Unidom::Price::Price, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
-    it_behaves_like 'validates', model_attributes, :amount,
-      {                            } => 0,
-      { amount: nil                } => 2,
-      { amount: ''                 } => 2,
-      { amount: '1'                } => 0,
-      { amount: 1                  } => 0,
-      { amount: 'A'                } => 1,
-      { amount: '1.23'             } => 0,
-      { amount: 1.23               } => 0,
-      { amount: '-0.01'            } => 1,
-      { amount: -0.01              } => 1,
-      { amount: '0'                } => 1,
-      { amount: 0                  } => 1,
-      { amount: '0.01'             } => 0,
-      { amount: 0.01               } => 0,
-      { amount: '999_999_999.99'   } => 0,
-      { amount: 999_999_999.99     } => 0,
-      { amount: '1_000_000_000'    } => 1,
-      { amount: 1_000_000_000      } => 1,
-      { amount: '1_000_000_000.01' } => 1,
-      { amount: 1_000_000_000.01   } => 1
+    it_behaves_like 'validates numericality', model_attributes, :amount,
+      range: 0..1_000_000_000, minimum_inclusive: true, maximum_inclusive: true
 
     it_behaves_like 'ProgneTapera::EnumCode', described_class.new(model_attributes), :calculation, Unidom::Price::Calculation
     it_behaves_like 'ProgneTapera::EnumCode', described_class.new(model_attributes), :charging,    Unidom::Price::Charging

6fd2d310c427d5b6a764829ae976d5a86e4fa0f2
Topbit Du <topbit.du@gmail.com>
2017-03-21 16:37:31 +0800

    1, Improve the As Priced concern for the argument validation.

--- app/models/unidom/price/concerns/as_priced.rb
+++ app/models/unidom/price/concerns/as_priced.rb
@@ -3,20 +3,31 @@
 
 module Unidom::Price::Concerns::AsPriced
 
-  extend ActiveSupport::Concern
+  extend  ActiveSupport::Concern
+  include Unidom::Common::Concerns::ArgumentValidation
 
   included do |includer|
 
     has_many :prices, class_name: 'Unidom::Price::Price', as: :priced
 
     def is_priced!(amount, by: nil, at: Time.now)
+
+      assert_present! :amount, amount
+      assert_present! :by,     by
+      assert_present! :at,     at
+
       pricing = prices.valid_at(now: at).alive.first
       pricing.soft_destroy if pricing.present?
       prices.create! pricer: by, amount: amount, opened_at: at
+
     end
 
     def is_priced?(at: Time.now)
+
+      assert_present! :at, at
+
       prices.valid_at(now: at).alive.exists?
+
     end
 
   end

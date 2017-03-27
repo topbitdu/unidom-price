3844c6cb4a204c27676e3dea54208b0ed0a68dd2
Topbit Du <topbit.du@gmail.com>
2017-03-27 21:19:05 +0800

    1, Improve the As Priced shared examples for the #is_priced! method, & the #is_priced? method.

--- lib/rspec/models/unidom/price/concerns/as_priced_shared_examples.rb
+++ lib/rspec/models/unidom/price/concerns/as_priced_shared_examples.rb
@@ -10,9 +10,9 @@ shared_examples 'Unidom::Price::Concerns::AsPriced' do |model_attributes, pricer
 
   context '#is_priced!' do
 
-    it 'should reject the amount argument = nil' do expect { @priced.is_priced! nil,     by: pricer, at: Time.now }.to raise_error(ArgumentError, 'The amount argument is required.') end
-    it 'should reject the by argument = nil'     do expect { @priced.is_priced! @amount, by: nil,    at: Time.now }.to raise_error(ArgumentError, 'The by argument is required.')     end
-    it 'should reject the at argument = nil'     do expect { @priced.is_priced! @amount, by: pricer, at: nil      }.to raise_error(ArgumentError, 'The at argument is required.')     end
+    #it 'should reject the amount argument = nil' do expect { @priced.is_priced! nil,     by: pricer, at: Time.now }.to raise_error(ArgumentError, 'The amount argument is required.') end
+    #it 'should reject the by argument = nil'     do expect { @priced.is_priced! @amount, by: nil,    at: Time.now }.to raise_error(ArgumentError, 'The by argument is required.')     end
+    #it 'should reject the at argument = nil'     do expect { @priced.is_priced! @amount, by: pricer, at: nil      }.to raise_error(ArgumentError, 'The at argument is required.')     end
 
     it "should be able to be priced by #{pricer.inspect}" do
       price = @priced.is_priced! @amount, by: pricer
@@ -32,7 +32,7 @@ shared_examples 'Unidom::Price::Concerns::AsPriced' do |model_attributes, pricer
 
   context '#is_priced?' do
 
-    it 'should reject the at argument = nil' do expect { @priced.is_priced? at: nil }.to raise_error(ArgumentError, 'The at argument is required.') end
+    #it 'should reject the at argument = nil' do expect { @priced.is_priced? at: nil }.to raise_error(ArgumentError, 'The at argument is required.') end
 
     it "should be able to be priced by #{pricer.inspect}" do
       expect(@priced.is_priced?).to be_falsey
@@ -63,6 +63,11 @@ shared_examples 'Unidom::Price::Concerns::AsPriced' do |model_attributes, pricer
       }
 
     it_behaves_like 'has_many', model_attributes, :prices, Unidom::Price::Price, [ price_1_attribtues, price_2_attribtues ]
+
+    model  = described_class.create! model_attributes
+    amount = 10.00
+    it_behaves_like 'assert_present!', model, :is_priced!, [ amount, { by: pricer, at: Time.now } ], [ { 0 => :amount }, :by, :at ]
+    it_behaves_like 'assert_present!', model, :is_priced?, [         {             at: Time.now } ], [                        :at ]
 
   end
 

7dab5e5cdc1b631ef8511dd820be07765f2f4a81
Topbit Du <topbit.du@gmail.com>
2017-03-30 21:25:59 +0800

    1, Improve the As Pricer shared examples for the coding style.

--- lib/rspec/models/unidom/price/concerns/as_pricer_shared_examples.rb
+++ lib/rspec/models/unidom/price/concerns/as_pricer_shared_examples.rb
@@ -1,17 +1,21 @@
 shared_examples 'Unidom::Price::Concerns::AsPricer' do |model_attributes|
 
-  price_1_attribtues = {
-      priced_id:   SecureRandom.uuid,
-      priced_type: 'Unidom::Price::Priced::Mock',
-      amount:      10.00
-    }
+  context do
 
-  price_2_attribtues = {
-      priced_id:   SecureRandom.uuid,
-      priced_type: 'Unidom::Price::Priced::Mock',
-      amount:      21.00
-    }
+    price_1_attribtues = {
+        priced_id:   SecureRandom.uuid,
+        priced_type: 'Unidom::Price::Priced::Mock',
+        amount:      10.00
+      }
 
-  it_behaves_like 'has_many', model_attributes, :pricings, Unidom::Price::Price, [ price_1_attribtues, price_2_attribtues ]
+    price_2_attribtues = {
+        priced_id:   SecureRandom.uuid,
+        priced_type: 'Unidom::Price::Priced::Mock',
+        amount:      21.00
+      }
+
+    it_behaves_like 'has_many', model_attributes, :pricings, Unidom::Price::Price, [ price_1_attribtues, price_2_attribtues ]
+
+  end
 
 end

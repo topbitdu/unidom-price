dae5fa6784dc70f53ca04f635745ab3bb1c2b620
Topbit Du <topbit.du@gmail.com>
2017-04-08 09:34:37 +0800

    1, Improve the As Pricer concern for the argument validations of the #price! method, & the #price? method.

--- app/models/unidom/price/concerns/as_pricer.rb
+++ app/models/unidom/price/concerns/as_pricer.rb
@@ -10,13 +10,24 @@ module Unidom::Price::Concerns::AsPricer
     has_many :pricings, class_name: 'Unidom::Price::Price', as: :pricer
 
     def price!(it, amount, at: Time.now)
+
+      assert_present! :it,     it
+      assert_present! :amount, amount
+      assert_present! :at,     at
+
       pricing = pricings.priced_is(it).valid_at(now: at).alive.first
       pricing.soft_destroy if pricing.present?
       pricings.create! priced: it, amount: amount, opened_at: at
+
     end
 
     def price?(it, at: Time.now)
+
+      assert_present! :it, it
+      assert_present! :at, at
+
       pricings.priced_is(it).valid_at(now: at).alive.exists?
+
     end
 
   end

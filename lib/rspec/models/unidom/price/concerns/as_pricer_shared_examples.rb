be67284638e199fa9420b422c3f8d7e6b3ebfacf
Topbit Du <topbit.du@gmail.com>
2017-04-10 09:44:46 +0800

    1, Improve the As Pricer shared examples for the #price! method, & the #price? method.

--- lib/rspec/models/unidom/price/concerns/as_pricer_shared_examples.rb
+++ lib/rspec/models/unidom/price/concerns/as_pricer_shared_examples.rb
@@ -1,4 +1,4 @@
-shared_examples 'Unidom::Price::Concerns::AsPricer' do |model_attributes|
+shared_examples 'Unidom::Price::Concerns::AsPricer' do |model_attributes, priced|
 
   context do
 
@@ -15,6 +15,11 @@ shared_examples 'Unidom::Price::Concerns::AsPricer' do |model_attributes|
       }
 
     it_behaves_like 'has_many', model_attributes, :pricings, Unidom::Price::Price, [ price_1_attribtues, price_2_attribtues ]
+
+    model  = described_class.create! model_attributes
+    amount = 10.00
+    it_behaves_like 'assert_present!', model, :price!, [ priced, amount, { at: Time.now } ], [ { 0 => :it }, { 1 => :amount }, :at ]
+    it_behaves_like 'assert_present!', model, :price?, [ priced,         { at: Time.now } ], [ { 0 => :it },                   :at ]
 
   end
 

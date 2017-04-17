f1a5217445ae0ff208c2db9cee3ba421ecae2346
Topbit Du <topbit.du@gmail.com>
2017-04-17 13:56:30 +0800

    1, 1. Improve the As Pricer shared examples for the #price! method, & the #price? method.

--- lib/rspec/models/unidom/price/concerns/as_pricer_shared_examples.rb
+++ lib/rspec/models/unidom/price/concerns/as_pricer_shared_examples.rb
@@ -1,5 +1,47 @@
 shared_examples 'Unidom::Price::Concerns::AsPricer' do |model_attributes, priced|
 
+  before :each do
+    @pricer = described_class.create! model_attributes
+    @amount = 10.00
+  end
+
+  after :each do
+  end
+
+  context '#price!' do
+
+    it "should be able to price #{priced.inspect} with amount #{@amount.inspect}" do
+      price = @pricer.price! priced, @amount
+      expect(price).to     be_present
+      expect(price).to     be_a(Unidom::Price::Price)
+      expect(price).not_to be_new_record
+    end
+
+    it "should be able to price #{priced.inspect} with amount #{@amount.inspect} at #{Time.now.inspect}" do
+      price = @pricer.price! priced, @amount, at: Time.now
+      expect(price).to     be_present
+      expect(price).to     be_a(Unidom::Price::Price)
+      expect(price).not_to be_new_record
+    end
+
+  end
+
+  context '#price?' do
+
+    it "should be able to price #{priced.inspect} with amount #{@amount.inspect}" do
+      expect(@pricer.price? priced).to be_falsey
+      @pricer.price!        priced, @amount
+      expect(@pricer.price? priced).to be_truthy
+    end
+
+    it "should be able to price #{priced.inspect} with amount #{@amount.inspect} at #{Time.now.inspect}" do
+      expect(@pricer.price? priced, at: Time.now).to be_falsey
+      @pricer.price!        priced, @amount, at: Time.now
+      expect(@pricer.price? priced, at: Time.now).to be_truthy
+    end
+
+  end
+
   context do
 
     price_1_attribtues = {

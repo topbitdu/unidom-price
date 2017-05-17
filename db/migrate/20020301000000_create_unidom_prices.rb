8995d21e5ec4a50027c4f3377b7279581afa4ff8
Topbit Du <topbit.du@gmail.com>
2017-05-17 22:07:53 +0800

    1, Improve the Price migration for the #state column.

--- db/migrate/20020301000000_create_unidom_prices.rb
+++ db/migrate/20020301000000_create_unidom_prices.rb
@@ -19,7 +19,7 @@ class CreateUnidomPrices < ActiveRecord::Migration
       t.text :description
       t.text :instruction
 
-      t.column   :state, 'char(1)', null: false, default: 'C'
+      t.column   :state, 'char(1)', null: false, default: Unidom::Common::STATE
       t.datetime :opened_at,        null: false, default: Unidom::Common::OPENED_AT
       t.datetime :closed_at,        null: false, default: Unidom::Common::CLOSED_AT
       t.boolean  :defunct,          null: false, default: false
